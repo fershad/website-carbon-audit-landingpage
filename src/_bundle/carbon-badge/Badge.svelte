f0cc295e0d67f5a550eb520872f2e32ccba6dc93
fishintaiwan <fershad@gmail.com>
2021-03-14 17:43:33 +0800

    update footer and carbon badge

--- src/_bundle/carbon-badge/Badge.svelte
+++ src/_bundle/carbon-badge/Badge.svelte
@@ -67,102 +67,26 @@
   const data = getCarbon();
 </script>
 
-<div id="wcb" class="carbon carbonbadge wcb-d">
+<div class="text-center">
   {#await data}
     <!-- data is pending -->
-    <div id="wcb_p">
-      <span id="wcb_g">Measuring CO<sub>2</sub>&hellip;</span>
-      <a
-        id="wcb_a"
-        target="_blank"
-        rel="noopener"
-        href="https://websitecarbon.com"
-      >Website Carbon</a>
-    </div>
+    <span>Measuring CO<sub>2</sub>&hellip;</span>
   {:then value}
     <!-- data was fulfilled -->
-    <div id="wcb_p">
-      <span id="wcb_g">{value.c}g of CO<sub>2</sub>/view</span>
-      <a
-        id="wcb_a"
-        target="_blank"
-        rel="noopener"
-        href="https://websitecarbon.com"
-      >Website Carbon</a>
-    </div>
-    <span id="wcb_2">Cleaner than {value.p}% of pages tested</span>
-  {:catch error}
-    <!-- data was rejected -->
-    <div id="wcb_p">
-      <span id="wcb_g">No results</span>
-      <a
-        id="wcb_a"
-        target="_blank"
-        rel="noopener"
-        href="https://websitecarbon.com"
-      >Website Carbon</a>
-    </div>
+    <p>
+      This pageview produced
+      <span class="marker-highlight" data-highlight="accent-light">{value.c}g of
+        CO<sub>2</sub></span>.
+    </p>
+    <small>It's cleaner than
+      {value.p}% of pages tested using the
+      <a target="_blank" rel="noopener" href="https://websitecarbon.com">Website
+        Carbon Calculator</a></small>
   {/await}
 </div>
 
 <style>
-  #wcb.carbonbadge {
-    font-size: 1rem;
-    text-align: center;
-    color: var(--text-color);
-    line-height: 1.15;
-    margin-bottom: 2rem;
-  }
-
-  #wcb.carbonbadge sub {
-    vertical-align: middle;
-    position: relative;
-    top: 0.3em;
-    font-size: 1rem;
-  }
-
-  #wcb #wcb_g,
-  #wcb #wcb_a,
-  #wcb #wcb_2 {
-    text-shadow: none;
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    /* font-size: 1rem; */
-    line-height: 1.15;
-    font-family: var(--base-font);
-    text-decoration: none;
-    margin: 0.2em 0;
-  }
-
-  #wcb #wcb_g,
-  #wcb #wcb_a {
-    padding: 0.3em 0.5em;
-    border: 0.13em solid var(--background-color);
-  }
-
-  #wcb #wcb_g {
-    border-radius: 0.3em 0 0 0.3em;
-    background: var(--background-color);
-    border-right: 0;
-    min-width: 8.2em;
-  }
-
-  #wcb #wcb_a {
-    border-radius: 0 0.3em 0.3em 0;
-    border-left: 0;
-    background: var(--color-accent-light);
-    color: var(--dark-text);
-    font-weight: 700;
-    border-color: var(--color-accent-light);
-    box-shadow: none;
-    left: -0.8em;
-    position: relative;
-  }
-
-  #wcb #wcb_2 {
-    color: var(--light-text);
-    font-size: 0.8rem;
+  p {
+    margin-bottom: 0.75ex;
   }
 </style>
